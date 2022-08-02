a63741bd12aff6012f65ce00342809c5bea8b239
Lucas Cantoni <lukcnt@outlook.com>
2022-08-02 00:14:11 -0300

    Added conditional which points if is a leap year or not.

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 # Input of the year that will be worked.
 year = int(input("Which year do you want to check? "))
+
+# Coniditional which points if is a leap year or not.
+if (year % 4) == 0:
+    if (year % 100) == 0:
+        if (year % 400) == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
